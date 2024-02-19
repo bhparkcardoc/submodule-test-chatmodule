49a83256814291cd53e191d8077ff50c270ff558
bh.park <bh.park.dev@gmail.com>
2024-02-19 16:52:03 +0900

    initial commit

---
+++ ChatModule/ChatModule/ChattingRoom.swift
@@ -0,0 +1,18 @@
+//
+//  ChattingRoom.swift
+//  ChatModule
+//
+//  Created by bh.park on 2/19/24.
+//
+
+import SwiftUI
+
+struct ChattingRoom: View {
+    var body: some View {
+        Text("Hello, I'm Chatting Room")
+    }
+}
+
+#Preview {
+    ChattingRoom()
+}
